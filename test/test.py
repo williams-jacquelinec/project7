83b6a479c662ee253957b73f63955678eb966bb8
Parker Grosjean <parker.grosjean@gmail.com>
2022-02-21 20:19:06 -0800

    Adding test skeleton.

---
+++ test/test.py
@@ -0,0 +1,45 @@
+# BMI 203 Project 7: Neural Network
+
+# Import necessary dependencies here
+
+
+# TODO: Write your test functions and associated docstrings below.
+
+def test_forward():
+    pass
+
+
+def test_single_forward():
+    pass
+
+
+def test_single_backprop():
+    pass
+
+
+def test_predict():
+    pass
+
+
+def test_binary_cross_entropy():
+    pass
+
+
+def test_binary_cross_entropy_backprop():
+    pass
+
+
+def test_mean_squared_error():
+    pass
+
+
+def test_mean_squared_error_backprop():
+    pass
+
+
+def test_one_hot_encode():
+    pass
+
+
+def test_sample_seqs():
+    pass
